b18e95ade404afef7dbcf9f713358bcc673097f7
Michael Del Casino <michael.delcasino@protonmail.ch>
2019-10-14 13:39:30 -0400

    Fix naming convention in cartTests

--- test/cartTests.js
+++ test/cartTests.js
@@ -3,11 +3,11 @@ const {Item} = require("../src/item");
 const {Cart, CartItem} = require("../src/cart");
 
 describe('Cart tests', function() {
-	var my_cart;
+	var cart;
 	var avocado;
 
 	beforeEach(function() {
-	 	my_cart = new Cart();
+	 	cart = new Cart();
 	 	avocado = new Item();	 	
 	 	avocado.setId("123");
 
@@ -16,35 +16,35 @@ describe('Cart tests', function() {
 	});	
 
 	it('test cart initially has no items', function() {
-		assert.equal(my_cart.size(), 0);
+		assert.equal(cart.size(), 0);
 	});
 
 	it('test add item to cart', function() {
-		my_cart.addItem(avocado, 2);
-		assert.equal(my_cart.size(), 1);
+		cart.addItem(avocado, 2);
+		assert.equal(cart.size(), 1);
 	});
 
 	it('test get item quantity', function() {
-		my_cart.addItem(avocado, 2);
-		assert.equal(my_cart.getItemQuantity(avocado), 2);
+		cart.addItem(avocado, 2);
+		assert.equal(cart.getItemQuantity(avocado), 2);
 	});
 
 	it('test remove item from cart', function() {
-		my_cart.addItem(avocado, 2);
-		my_cart.removeItem(avocado);
-		assert.equal(my_cart.size(), 0);
+		cart.addItem(avocado, 2);
+		cart.removeItem(avocado);
+		assert.equal(cart.size(), 0);
 	});
 
 	it('test remove all items from cart', function() {
-		my_cart.addItem(avocado, 2);
-		my_cart.addItem(apple, 2);
-		my_cart.removeAllItems();
-		assert.equal(my_cart.size(), 0);
+		cart.addItem(avocado, 2);
+		cart.addItem(apple, 2);
+		cart.removeAllItems();
+		assert.equal(cart.size(), 0);
 	});
 
 	it('test cart subtotal', function() {
 		avocado.setCost(100);
-		my_cart.addItem(avocado, 2);
-		assert.equal(my_cart.getSubtotal(), 200);
+		cart.addItem(avocado, 2);
+		assert.equal(cart.getSubtotal(), 200);
 	});
 });

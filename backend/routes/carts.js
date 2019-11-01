9880bf238f8e4f359f6f05d47186fc027187d2bd
Aaron Cronin <ac6296@nyu.edu>
2019-11-01 16:46:07 -0400

    Mongo Cart Class updates

--- backend/routes/carts.js
+++ backend/routes/carts.js
@@ -9,39 +9,50 @@ router.route('/').get((req, res) => {
 
 router.route('/add').post((req, res) => {
     const username = req.body.username;
-    const num_items = Number(req.body.num_items);
-    const alias_list = req.body.alias_list;
+    var alias_list = req.body.alias_list;
+    var num_items = alias_list.length;
 
-    const newCart = new Cart({
+    var newCart = new Cart({
         username,
         num_items,
         alias_list
     });
 
-    newCart.save()
+    return newCart.save()
     .then(() => res.json('Cart added!'))
     .catch(err => res.status(400).json('Error: '+err));
 });
 
 
-router.route('/Cart/:username').get((req, res) => {
-    Cart.find({username: req.params.username})
+router.route('/:username/showCart').get((req, res) => {
+    Cart.find({username: req.body.username})
     .then(cart => res.json(cart))
     .catch(err => res.status(400).json('Error ' + err));
 });
 
-router.route('/update/:username').post((req, res) => {
-    Cart.find({username: req.params.username})
-    .then(cart => {
-        cart.username = req.body.username;
-        cart.num_items = Number(req.body.num_items);
-        cart.alias_list = req.body.alias_list;
+router.route('/:username/updateList').get((req, res) => {
+    Cart.find({username: req.body.username})
+    .then(cart => 
+        res.json(cart.keys)
+        //cart.save()
+    )
+    .catch(err => res.status(400).json('Error '+ err));
+    
+    //cart.alias_list.push(req.body.alias_list);
+    
+    //cart.alias_list.push(req.body.add_item)
+    /*
+    .then({
+
+        cart.alias_list.append(req.body.alias_list);
 
         cart.save()
         .then(() => res.json('Cart updated!'))
         .catch(err => res.status(400).json('Error ' + err));
     })
-    .catch(err => res.status(400).json('Error '+ err));
+    */
+    //.then(() => res.json('Cart updated!'))
+    //.catch(err => res.status(400).json('Error '+ err));
 });
 
 

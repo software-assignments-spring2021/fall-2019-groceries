e8c9cd4c13c0058255255e625e396924f1f6f2d5
don miguel <michael.delcasino@protonmail.ch>
2019-11-04 02:00:28 +0000

    Update aliases.js

--- backend/routes/aliases.js
+++ backend/routes/aliases.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 let User = require('../models/user.model');
 
 router.route('/update/:username').post((req,res) => {
-    User.find({username: req.body.username})
+    User.find({username: req.params.username})
     .updateOne({$set: {aliases: req.params.aliases}})
     .then(user => res.json())
     .catch(err => res.status(400).json('Error '+ err));

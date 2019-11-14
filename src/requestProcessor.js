0396aeb7d396a42e0981cb8bffe7dced6d56e0cb
Michael Del Casino <michael.delcasino@protonmail.ch>
2019-11-14 14:41:05 -0500

    Add request/response flow for product search

--- src/requestProcessor.js
+++ src/requestProcessor.js
@@ -1,4 +1,4 @@
-const {AddCartItemResponse, DisplayUserAliasesResponse, DisplayUserCartResponse} = require("./userResponses");
+const {AddCartItemResponse, DisplayProductSearchResponse, DisplayUserAliasesResponse, DisplayUserCartResponse} = require("./userResponses");
 var sleep = require('system-sleep');
 
 class RequestProcessor {
@@ -18,6 +18,60 @@ class RequestProcessor {
 
 	setMarketplace(marketplace) {
 		this.marketplace = marketplace;
+	}
+
+	onDisplayProductSearchRequest(request) {
+		var userResponse = new DisplayProductSearchResponse();
+		const user = request.getUser();
+		userResponse.setUser(user);
+
+		if (user === null) {
+			userResponse.setResponseText("User is not defined");
+			this.bot.onDisplayProductSearchResponse(userResponse);
+    		return;
+		} 
+		
+		const productName = request.getProductName();
+
+		if (productName === null) {
+			userResponse.setResponseText("Product name is null");
+			this.bot.onDisplayProductSearchResponse(userResponse);
+    		return;
+		}
+
+		const productSearchResults = this.marketplace.getProductSearchResults(productName);
+
+		if (productSearchResults === null) {
+			userResponse.setResponseText("Product search for " + productName + " failed.");
+		}
+		else {
+			userResponse.setResponseText(productSearchResults);
+		}
+
+		this.bot.onDisplayProductSearchResponse(userResponse);
+	}
+
+	onDisplayUserAliasesRequest(request) {
+		var userResponse = new DisplayUserAliasesResponse();
+		const user = request.getUser();
+		userResponse.setUser(user);	
+		
+    	if (user === null) {
+			userResponse.setResponseText("User is not defined");
+			this.bot.onDisplayUserAliasesResponse(userResponse);
+    		return;
+    	}    	
+		
+		const userAliases = this.database.getUserAliases(user);
+
+    	if (userAliases === null) {
+    		userResponse.setResponseText("User does not have any aliases defined");
+    	}
+    	else {
+    		userResponse.setResponseText(userAliases);
+    	}    	
+
+    	this.bot.onDisplayUserAliasesResponse(userResponse);
 	}
 
 	onDisplayUserCartRequest(request) {
@@ -43,29 +97,6 @@ class RequestProcessor {
         // send response to bot without blocking
         this.bot.onDisplayUserCartResponse(userResponse);
     }
-
-    onDisplayUserAliasesRequest(request) {
-		var userResponse = new DisplayUserAliasesResponse();
-		const user = request.getUser();
-		userResponse.setUser(user);	
-		
-    	if (user === null) {
-			userResponse.setResponseText("User is not defined");
-			this.bot.onDisplayUserAliasesResponse(userResponse);
-    		return;
-    	}    	
-		
-		const userAliases = this.database.getUserAliases(user);
-
-    	if (userAliases === null) {
-    		userResponse.setResponseText("User does not have any aliases defined");
-    	}
-    	else {
-    		userResponse.setResponseText(userAliases);
-    	}    	
-
-    	this.bot.onDisplayUserAliasesResponse(userResponse);
-	}
 }
 
 module.exports = {

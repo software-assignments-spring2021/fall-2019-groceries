3145df41323f44067253b927db4a4fbe1b28faa0
Michael Del Casino <michael.delcasino@protonmail.ch>
2019-12-11 11:52:41 -0500

    Add test for OrderStatusRetriever

--- src/orderStatusRetriever.js
+++ src/orderStatusRetriever.js
@@ -6,13 +6,13 @@ class OrderStatusRetriever {
             var user = "570FF481878B49158B137BD7";
             var password = '';
             var base64encodedData = new Buffer.from(user + ':' + password).toString('base64');
-            var url = "https://api.zinc.io/v1/orders?limit=10&retailer=amazon";             
+            var url = "https://api.zinc.io/v1/orders?limit=500&retailer=amazon";             
     
             var xhr = new XMLHttpRequest();  
             xhr.open("GET", url, false );
             xhr.setRequestHeader('Authorization', 'Basic '+ base64encodedData);
             xhr.responseType = 'json';
-            xhr.onload = () => {return resolve(JSON.parse(xhr.responseText))};
+            xhr.onload = () => {return resolve(JSON.parse(xhr.responseText)['orders'])};
             xhr.error = () => {return reject()};
             xhr.send();       
         });

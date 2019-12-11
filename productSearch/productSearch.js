e531435f89e944cf353b1ea8f7030165fb0d3baa
Michael Del Casino <michael.delcasino@protonmail.ch>
2019-12-10 20:29:19 -0500

    Fix /add use of search

--- productSearch/productSearch.js
+++ productSearch/productSearch.js
@@ -1,38 +1,17 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-    
 global.Buffer = global.Buffer || require('buffer').Buffer;
 
-    module.exports = {
-        
-        searchItem: function(query){
-    
-
+module.exports = {
+    searchItem: function(query) {
         var user = "570FF481878B49158B137BD7";
         var password = '';
         var base64encodedData = new Buffer.from(user + ':' + password).toString('base64');
-
-
-        var url = "https://api.zinc.io/v1/search?query="+query+"&page=1&retailer=amazon";
-                
-       
+        var url = "https://api.zinc.io/v1/search?query="+query+"&page=1&retailer=amazon";             
         xmlHttp = new XMLHttpRequest();  
-        xmlHttp.open( "GET", url, false );
+        xmlHttp.open("GET", url, false );
         xmlHttp.setRequestHeader('Authorization', 'Basic '+base64encodedData);
         xmlHttp.responseType = 'json';
-        xmlHttp.send();
-
-        var count = Object.keys(xmlHttp.responseText).length;
-
-       
+        xmlHttp.send();       
         return xmlHttp;
-
-        }
-
-    
-
     }
-
-
-    
-
-
+}
